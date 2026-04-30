002b301a286af5d8d410ecf4e55d3cd0b60bb11a
Alu-08 <malumine08@gmail.com>
2026-04-30 11:04:54 -0300

    hola

--- src/app/aut/inicio-secion/inicio-secion.ts
+++ src/app/aut/inicio-secion/inicio-secion.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-secion',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio-secion.html',
   styleUrl: './inicio-secion.css',
 })
