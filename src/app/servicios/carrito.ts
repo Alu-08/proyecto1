2f7bb20537c22180e8ad65e62ac667472ed5d237
Alu-08 <malumine08@gmail.com>
2026-05-20 11:02:25 -0300

    servicios

---
+++ src/app/servicios/carrito.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Carrito {
+  obtenerProducto(id: number): void {
+    //const producto = document.getElementById(id);
+  }
+  añadirProducto(): void {
+  } 
+  editarProducto(): void {
+  }
+  eliminarProducto(): void {
+  } 
+}
